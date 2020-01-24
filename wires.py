6853f5dcc7c5446205a5ab4e4d54538ebb223274
Marcus Rönnbäck <zappmarcus97@gmail.com>
2020-01-24 21:58:32 +0100

    changed the folder structure

---
+++ wires.py
@@ -0,0 +1,28 @@
+# Simple wires
+
+print("Input the wires top to bottom. (y, bla , blue, g, r, w)")
+wires = input()
+wire = list(wires)
+
+if len(wire) == 3:
+    for w in wire:
+        if w != "r":
+            print("Cut the second wire")
+    if wire[2] == "w"
+        print("Cut the last wire")
+    count = 0
+    for w in wire:
+        if w == "blue":
+            count +=1
+    
+
+
+
+
+Yellow
+Black
+Blue
+Green
+White
+
+
